a12a82704f0233261b5c8b9c929aa1d4a8a70d5b
mrpoundsign <mrpoundsign@4f8e31e1-f535-0410-b198-f7564c4c0b32>
2007-10-19 22:30:04 +0000

    Slight stylistic change from open brackets on newlines to them being on the end of line.

--- trunk/ProxyHandler.class.php
+++ trunk/ProxyHandler.class.php
@@ -12,25 +12,21 @@ class ProxyHandler
     private $pragma=false;
     private $client_headers=array();
 
-    function __construct($url, $proxy_url)
-    {
+    function __construct($url, $proxy_url) {
         // Strip the trailing '/' from the URLs so they are the same.
         $this->url = rtrim($url,'/');
         $this->proxy_url =  rtrim($proxy_url,'/');
 
         // Parse all the parameters for the URL
-        if (isset($_SERVER['PATH_INFO']))
-        {
+        if (isset($_SERVER['PATH_INFO'])) {
             $proxy_url .= $_SERVER['PATH_INFO'];
         }
-        else
-        {
+        else {
             // Add the '/' at the end
             $proxy_url .= '/';
         }
 
-        if ($_SERVER['QUERY_STRING'] !== '')
-        {
+        if ($_SERVER['QUERY_STRING'] !== '') {
             $proxy_url .= "?{$_SERVER['QUERY_STRING']}";
         }
 
@@ -46,8 +42,7 @@ class ProxyHandler
         $this->setCurlOption(CURLOPT_HEADERFUNCTION, array($this,'readHeaders'));
 
         // Process post data.
-        if (count($_POST))
-        {
+        if (count($_POST)) {
             // Empty the post data
             $post=array();
 
@@ -55,8 +50,7 @@ class ProxyHandler
             $this->setCurlOption(CURLOPT_POST, true);
 
             // Encode and form the post data
-            foreach($_POST as $key=>$value)
-            {
+            foreach($_POST as $key=>$value) {
                 $post[] = urlencode($key)."=".urlencode($value);
             }
 
@@ -64,8 +58,7 @@ class ProxyHandler
 
             unset($post);
         }
-        elseif ($_SERVER['REQUEST_METHOD'] !== 'GET') // Default request method is 'get'
-        {
+        elseif ($_SERVER['REQUEST_METHOD'] !== 'GET') // Default request method is 'get' {
             // Set the request method
             $this->setCurlOption(CURLOPT_CUSTOMREQUEST, $_SERVER['REQUEST_METHOD']);
         }
@@ -75,61 +68,50 @@ class ProxyHandler
 
     }
 
-    public function setClientHeader($header)
-    {
+    public function setClientHeader($header) {
         $this->client_headers[] = $header;
     }
 
     // Executes the proxy.
-    public function execute()
-    {
+    public function execute() {
         $this->setCurlOption(CURLOPT_HTTPHEADER, $this->client_headers);
         curl_exec($this->curl_handler);
     }
 
     // Get the information about the request.
     // Should not be called before exec.
-    public function getCurlInfo()
-    {
+    public function getCurlInfo() {
         return curl_getinfo($this->curl_handler);
     }
 
     // Sets a curl option.
-    public function setCurlOption($option, $value)
-    {
+    public function setCurlOption($option, $value) {
         curl_setopt($this->curl_handler, $option, $value);
     }
 
-    protected function readHeaders(&$cu, $string)
-    {
+    protected function readHeaders(&$cu, $string) {
         $length = strlen($string);
-        if (preg_match(',^Location:,', $string))
-        {
+        if (preg_match(',^Location:,', $string)) {
             $string = str_replace($this->proxy_url, $this->url, $string);
         }
-        elseif(preg_match(',^Cache-Control:,', $string))
-        {
+        elseif(preg_match(',^Cache-Control:,', $string)) {
             $this->cache_control = true;
         }
-        elseif(preg_match(',^Pragma:,', $string))
-        {
+        elseif(preg_match(',^Pragma:,', $string)) {
             $this->pragma = true;
         }
-        if (header !== "\r\n")
-        {
+        if (header !== "\r\n") {
             header(rtrim($string));
 
         }
         return $length;
     }
     
-    protected function handleClientHeaders()
-    {
+    protected function handleClientHeaders() {
         $headers = apache_request_headers();
 
         foreach ($headers as $header => $value) {
-            switch($header)
-            {
+            switch($header) {
                 case 'Host':
                     break;
                 default:
@@ -139,20 +121,16 @@ class ProxyHandler
         } 
     }
 
-    protected function readResponse(&$cu, $string)
-    {
+    protected function readResponse(&$cu, $string) {
         static $headersParsed = false;
 
         // Clear the Cache-Control and Pragma headers
         // if they aren't passed from the proxy application.
-        if ($headersParsed === false)
-        {
-            if (!$this->cache_control)
-            {
+        if ($headersParsed === false) {
+            if (!$this->cache_control) {
                 header('Cache-Control: ');
             }
-            if (!$this->pragma)
-            {
+            if (!$this->pragma) {
                 header('Pragma: ');
             }
             $headersParsed = true;
